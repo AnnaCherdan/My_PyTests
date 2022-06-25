ed183112d5d16c8a53144a2d6968954840ed49a5
AnnaCherdan <anacher@mail.ru>
2022-06-25 23:29:37 +0700

    Logo and key tests.

--- my_pet_api/my_pet.py
+++ my_pet_api/my_pet.py
@@ -1,6 +1,19 @@
-from config_my_pet import key_param
+import requests
+import json
+from config_my_pet import log_key_param
 
 
-def test_key_param(key_param):
-    res = key_param
-    assert res.status_code == 200
+def test_logo():
+    logo_key = requests.get(url='https://petfriends.skillfactory.ru/login',
+                            data={"email": "anchetest@mail.ru", "password": "ljkmvtyf"})
+    assert logo_key.status_code == 200
+
+
+def test_apikey(log_key_param):
+    header = {"accept": "application/json", "email": "anchetest@mail.ru", "password": "ljkmvtyf"}
+    apikey = requests.get(url='https://petfriends.skillfactory.ru/api/key',
+                          headers=header)
+    print(apikey.text)
+    assert apikey.status_code == 200
+    assert list(json.loads(apikey.text))
+    print(list(json.loads(apikey.text)))
