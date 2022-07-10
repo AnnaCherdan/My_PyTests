2ee175da8b8f0ad5b1e87b74d189cdc43e3fa0eb
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-07-10 17:22:17 +0700

    good day, selenium.

--- selen_locator_25/test_25.py
+++ selen_locator_25/test_25.py
@@ -1,9 +1,9 @@
 import time
 
+# class TestPF:
+
 
 def test_login(driver, get_chrome_options):
-    driver.get('https://petfriends.skillfactory.ru/login')
-
     time.sleep(5)
 
     search_input = driver.find_element_by_id('email')
@@ -18,4 +18,28 @@ def test_login(driver, get_chrome_options):
     time.sleep(5)
 
     driver.save_screenshot('result.png')
+
+
+def test_path(driver):
+    time.sleep(5)
+    search_input = driver.find_element_by_xpath('//*[@id="email"]')
+    search_input.send_keys('anchetest@mail.ru')
+
+    search_input = driver.find_element_by_xpath('//*[@id="pass"]')
+    search_input.send_keys('ljkmvtyf')
+
+    time.sleep(5)
+
+    search_button = driver.find_element_by_xpath('//body/div[1]/div[1]/form[1]/div[3]/button[1]')
+    search_button.submit()
+
+
+def test_path_drive(driver):
+    time.sleep(3)
+    assert driver.find_element_by_tag_name('h2').text == "AnnaCherdan"
+    time.sleep(3)
+
+    driver.close()
+
+
 
