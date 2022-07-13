932034122f578ec4e6cb945fda27e5507fc2fab3
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-07-13 18:05:54 +0700

    good day, selenium.

--- veb_25/confveb.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from selenium import webdriver
-
-
-@pytest.fixture
-def get_webdriver(request):
-    driver = webdriver.Chrome('D:\\AllDoc\\AnnCherdan\\webdrivers\\chromedriver.exe')
-    return driver
