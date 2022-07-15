b1f5c3b173ee9b145a5ce02c7825d3b1c1bc8533
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-07-15 19:24:58 +0700

    good day, selenium.

--- vebinar_25/conftest.py
+++ vebinar_25/conftest.py
@@ -1,6 +1,14 @@
 import pytest
 from selenium import webdriver
-import data_base
+from selenium.webdriver.chrome.options import Options
+
+
+@pytest.fixture
+def chrome_options(chrome_options):
+    chrome_options = Options()
+    # chrome_options.set_headless(True)
+    chrome_options.add_argument('--headless')
+    return chrome_options
 
 
 @pytest.fixture
@@ -13,7 +21,7 @@ def driver_labirint(request):
 @pytest.fixture(autouse=True)
 def driver_pikabu(request):
     driver_pik = webdriver.Chrome('D:\\AllDoc\\AnnCherdan\\Python\\chromedriver.exe')
-    # driver_pik.get('https://pikabu.ru/')
+    driver_pik.get('https://pikabu.ru/')
     return driver_pik
 
 
@@ -21,8 +29,8 @@ def driver_pikabu(request):
 def driver_friends(request):
     drifriend = webdriver.Chrome('D:\\AllDoc\\AnnCherdan\\Python\\chromedriver.exe')
     drifriend.get('https://petfriends.skillfactory.ru/login')
-    # drifriend.find_element_by_xpath('//input[@id="email"]').send_keys('anchetest@mail.ru')
-    # drifriend.find_element_by_xpath('//*[@id="pass"]').send_keys('ljkmvtyf')
-    # drifriend.find_element_by_xpath('//body/div[1]/div[1]/form[1]/div[3]/button[1]').submit()
-    # drifriend.find_element_by_xpath('//*[@id="navbarNav"]/ul[1]/li[1]/a[1]').click()
+    drifriend.find_element_by_xpath('//input[@id="email"]').send_keys('anchetest@mail.ru')
+    drifriend.find_element_by_xpath('//*[@id="pass"]').send_keys('ljkmvtyf')
+    drifriend.find_element_by_xpath('//body/div[1]/div[1]/form[1]/div[3]/button[1]').submit()
+    drifriend.find_element_by_xpath('//*[@id="navbarNav"]/ul[1]/li[1]/a[1]').click()
     return drifriend
--- vebinar_25/lab_test.py
+++ vebinar_25/lab_test.py
@@ -2,8 +2,9 @@ import time
 
 
 def test_1(driver_friends):
-    time.sleep(3)
-    # assert driver_friends.find_element_by_xpath('//*[@id="pass"]').send_keys('gjhfjhjh')
+    driver_friends.implicitly_wait(10)
+    # assert driver_pikabu.find_element_by_xpath('//*[@id="adfox_156570562144159249"]/div[1]/a[1]')\
+    #     .is_displayed(), "AHTUNG VISIBLE"
+    driver_friends.implicitly_wait(10)
     # assert driver_friends.find_element_by_xpath('//body/div[1]/div[1]/h1[1]').is_displayed, 'Все сломалось!'
-    # time.sleep(3)
     driver_friends.close()
